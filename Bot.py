c264b824effac21192e2e11eb2aaf2f3a37304ab
Sib <chengjie.michael.huang@gmail.com>
2022-10-05 23:36:38 -0700

    created waitlist class

--- Bot.py
+++ Bot.py
@@ -1,12 +1,14 @@
 # https://discordpy.readthedocs.io/en/stable/index.html#
 
+# send a msg to a specific channel
+# https://www.google.com/search?q=discord.py+sending+a+message+in+a+channel&rlz=1C1RXQR_enCA978CA978&oq=discord.py+sending+a+message+in+a+channel&aqs=chrome..69i57.6491j0j7&sourceid=chrome&ie=UTF-8#kpvalbx=_gXY-Y5_xObCT0PEPmNKL-Aw_18
+
 import math
-from queue import PriorityQueue
 import discord
+from Discord.WaitListBot.WaitList import PriorityItem, WaitList
 
-from Discord.PriorityQueueBot.Utils import get_priority
-from Discord.PriorityQueueBot.WaitList import WaitList
-
+from Utils import get_priority
+# from WaitList import WaitList
 '''
     Post weekly announcement msg
     Create/reset priority queue
@@ -16,10 +18,11 @@ from Discord.PriorityQueueBot.WaitList import WaitList
 
 
 class WaitListBot(discord.Client):
+
     def __init__(self, announcementChannel):
-        self.waitList = WaitList()
         self.announcementChannel = announcementChannel
         self.announcementID = -1
+        self.wait_list = WaitList()
 
     def is_announcement(self, msgId):
         if self.announcementID != -1 and msgId == self.announcementID:
@@ -37,25 +40,25 @@ class WaitListBot(discord.Client):
     async def send(self, channel, message):
         await self.send_message(self.get_channel(channel), message)
 
-    async def on_reaction_added(self, reaction, user):
-        if self.is_announcement(reaction.message):
-            priority = get_priority(user.roles)
-            entry = {'priority': priority, 'name': user.name}
-            self.waitList.add(entry)
-            waitlist = self.waitList.get()
-            await self.send(self.announcementChannel, waitlist)
+    # async def on_reaction_added(self, reaction, user):
+    #     if self.is_announcement(reaction.message):
+    #         priority = get_priority(user.roles)
+    #         entry = PriorityItem(priority, user.name)
+    #         self.wait_list.add(entry)
+    #         wait_list = self.wait_list.get()
+    #         await self.send(self.announcementChannel, wait_list)
 
-    async def on_reaction_remove(self, reaction, user):
-        if self.is_announcement(reaction.message):
-            priority = get_priority(user.roles)
-            entry = {'priority': priority, 'name': user.name}
-            self.waitList.remove(entry)
-            waitlist = self.waitList.get()
-            await self.send(self.announcementChannel, waitlist)
+    # async def on_reaction_remove(self, reaction, user):
+    #     if self.is_announcement(reaction.message):
+    #         priority = get_priority(user.roles)
+    #         entry = {'priority': priority, 'name': user.name}
+    #         self.waitList.remove(entry)
+    #         waitlist = self.waitList.get()
+    #         await self.send(self.announcementChannel, waitlist)
 
 
-intents = discord.Intents.default()
-intents.message_content = True
+# intents = discord.Intents.default()
+# intents.message_content = True
 
-client = WaitListBot(intents=intents)
-client.run()
+# client = WaitListBot(intents=intents)
+# client.run()
--- WaitList.py
+++ WaitList.py
@@ -1,22 +1,99 @@
+class PriorityItem():
+
+    def __init__(self, priority: int, value: str) -> None:
+        self.priority = priority
+        self.value = value
+
+
 class WaitList():
     # sort queue from low to high priority key, items poppable from middle
     # untested
-    def __init__(self):
-        self.waitList = []
+    def __init__(self, wait_priority=2, max_priority=5, max_capacity=20):
+        self.lst = []
+        self.wait_list = []
+        self.wait_priority = wait_priority
+        self.max_priority = max_priority
+        self.max_capacity = max_capacity
 
     def add(self, item):
-        for i in range(len(self.waitList)):
-            if item['priority'] < self.waitList['priority']:
-                self.waitList.insert(i, item)
-                return
-        self.waitList.append(item)
+        if item.priority > self.wait_priority:
+            self.wait_list.append(item)
+            return
+        if len(self.lst) < self.max_capacity:
+            self.lst.append(item)
+            return
+        self.wait_list.append(item)
 
     def remove(self, item):
-        self.waitList.remove(item)
+        for i, v in enumerate(self.lst):
+            if v.value == item.value:
+                self.lst.pop(i)
+                return
+        for i, v in enumerate(self.wait_list):
+            if v.value == item.value:
+                self.wait_list.pop(i)
+                return
+        print(f'Error: {item.value} not found and cannot be removed')
 
     def show(self):
-        for entry in self.waitList:
-            print(entry)
+        print('List')
+        for entry in self.lst:
+            print(entry.value)
+        for entry in self.wait_list:
+            print('waitlisted: ', entry.value)
+        print()
+
+    def show_absolute(self):
+        print('Absolute List')
+        for entry in self.lst:
+            print(entry.value)
+        free_spots = self.max_capacity - len(self.lst)
+        if free_spots <= 0:
+            print()
+            return
+        for entry in self.wait_list:
+            print(entry.value)
+            free_spots -= 1
+            if free_spots <= 0:
+                print()
+                return
+        print()
 
     def get(self):
-        return self.waitList
+        wait_list = ''
+        for entry in self.lst:
+            wait_list += entry.value + '\n'
+        for entry in self.wait_list:
+            wait_list += 'waitlisted: ' + entry.value + '\n'
+        return wait_list
+
+    def get_absolute(self):
+        wait_list = ''
+        for entry in self.lst:
+            wait_list += entry.value + '\n'
+        free_spots = self.max_capacity - len(self.lst)
+        if free_spots <= 0:
+            return wait_list
+        for entry in self.wait_list:
+            wait_list += entry.value + '\n'
+            free_spots -= 1
+            if free_spots <= 0:
+                return wait_list
+        return wait_list
+
+
+if __name__ == '__main__':
+    waitlist = WaitList()
+    waitlist.add(PriorityItem(0, 'Michael'))
+    waitlist.add(PriorityItem(0, 'Johnny'))
+    waitlist.add(PriorityItem(1, 'Karen'))
+    waitlist.add(PriorityItem(2, 'Andy'))
+    waitlist.add(PriorityItem(3, 'Brandon'))
+    waitlist.add(PriorityItem(3, 'Clark'))
+    waitlist.show()
+    waitlist.show_absolute()
+    waitlist.remove(PriorityItem(0, 'Michael'))
+    waitlist.remove(PriorityItem(0, 'Brandon'))
+    waitlist.show()
+    waitlist.show_absolute()
+    pass
